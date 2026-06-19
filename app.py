c64cfe900d886085372a8eacd64acd04443cfa98
406-30-teepakron <54653@yupparaj.ac.th>
2026-06-19 10:28:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def page2():
     st.title("Second page")
 
 pg = st.navigation([
-    st.Page("page1.py", title="First page", icon="🔥"),
+    st.Page("app.py", title="First page", icon="🔥"),
     st.Page(page2, title="Second page", icon=":material/favorite:"),
     st.Page(
         "https://docs.streamlit.io",
