6b5eab5dd47c0b8f8f8b520ecaaa417ea0a43421
406-30-teepakron <54653@yupparaj.ac.th>
2026-06-26 10:27:50 +0700

    Update digital_profile.py

--- digital_profile.py
+++ digital_profile.py
@@ -1,12 +1,8 @@
 import streamlit as st
-
-# ส่วนหัวข้อเว็บ (Header)
 st.title("My digita profile ✨")
 st.header("แนะนำตัว")
-
 st.write("นายทีปกร พลหาญ เรียนดี มีความรู้")
 st.write("**ชื่อเล่น อะตอม**")
 st.write("**ชั้น ม.4/6** เลขที่ 30")
-
 st.header("เป้าหมายและสิ่งที่อยากทำ")
 st.write("เป็นนักธุรกิจ")
