4c26fead556e1ec1ac4866743505dfbe6ef2348d
406-30-teepakron <54653@yupparaj.ac.th>
2026-07-10 10:29:39 +0700

    Update conyear.py

--- conyear.py
+++ conyear.py
@@ -5,3 +5,7 @@ bh_year=st.number_input("กรอกปี พ.ศ. ที่ต้องกา
 ce_year=bh_year-543
 st.header(f"ปี ค.ศ. คือ : {ce_year}")
 #f = เชื่อมการแสดงผลตัวเลขกับตัวหนังสือ
+
+ce_year2=st.number_input("กรอกปี ค.ศ. ที่ต้องการแปลง",value=2026)
+bh_year2=ce_year2+543
+st.header(f"ปี พ.ศ. คือ : {bh_year2}")
