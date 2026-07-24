f2f24077018c81a29744e8d0381f841405b53660
406-30-teepakron <54653@yupparaj.ac.th>
2026-07-24 10:37:29 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.title("แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
 price=st.number_input("ระบุราคาราคา")
 vat=price*7/100
-st.header(f"ภาษีมูลค่าเพิ่ม (VAT 7%) : {vat} บาท")
-st.header(f"ราคาสุทธิ : {price-vat} บาท")
+st.header(f"ภาษีมูลค่าเพิ่ม (VAT 7%) : {vat:.2f} บาท")
+st.header(f"ราคาสุทธิ : {price-vat:.2f} บาท")
 st.divider()
 st.write("นายทีปกร พลหาญ เลขที่30 4/6")
